602edc15fffdb7812b16a03cc7e5c6526c35b955
Filip Hracek <filip.hracek@gmail.com>
2024-08-11 11:48:50 +0200

    Remove extraneous comment

--- bench_godot/wobble.gd
+++ bench_godot/wobble.gd
@@ -10,7 +10,6 @@ func _ready():
 	transformMatrix = transformMatrix.scaled_local(Vector2(scale, scale))
 	transformMatrix = transformMatrix.translated(Vector2(960 * 0.4, 600 * 0.12))
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	age += delta
 	
